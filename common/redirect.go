6be0358fee37815ac31f9c63f93042418e3e311f
Jeongin Lee <jil8885@hanyang.ac.kr>
2021-11-15 11:28:01 +0900

    Secondary 서버일 경우 Primary 서버에 요청 후 실패하면 동작하도록 수정

--- common/redirect.go
+++ common/redirect.go
@@ -1,1 +1,13 @@
 package common
+
+import "os"
+
+func GetPrimaryServer() string{
+	isPrimary, keyExists := os.LookupEnv("is_primary")
+	primaryServer, addressExists := os.LookupEnv("primary_server")
+	if keyExists && addressExists && isPrimary == "False" {
+		return primaryServer
+	} else {
+		return ""
+	}
+}
