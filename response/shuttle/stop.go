53538cc57a9b7d3d7ba1bbe8ed42ea69929c6102
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-12-13 09:41:07 +0900

    feat: 셔틀버스 정류장 항목 조회

--- response/shuttle/stop.go
+++ response/shuttle/stop.go
@@ -13,25 +13,21 @@ type StopListItem struct {
 	Location StopLocation `json:"location"`
 }
 
-//type RouteItemResponse struct {
-//	Name        string           `json:"name"`
-//	Description RouteDescription `json:"description"`
-//	StopList    []RouteStopItem  `json:"stop_list"`
-//}
+type StopItemResponse struct {
+	Name      string          `json:"name"`
+	Location  StopLocation    `json:"location"`
+	RouteList []StopRouteItem `json:"route"`
+}
 
 type StopLocation struct {
 	Latitude  float64 `json:"latitude"`
 	Longitude float64 `json:"longitude"`
 }
 
-//type RouteStopItem struct {
-//	Name          string   `json:"name"`
-//	TimetableList []string `json:"timetable"`
-//}
-//
-//type RouteLocationResponse struct {
-//	Location []float64 `json:"location"`
-//}
+type StopRouteItem struct {
+	Name          string   `json:"name"`
+	TimetableList []string `json:"timetable"`
+}
 
 func CreateStopListResponse(stopList []shuttle.StopItem) StopListResponse {
 	var stop []StopListItem
@@ -47,72 +43,21 @@ func CreateStopListResponse(stopList []shuttle.StopItem) StopListResponse {
 	return StopListResponse{Stop: stop}
 }
 
-//func CreateRouteItemResponse(routeItem shuttle.Route) RouteItemResponse {
-//	var routeStopList []RouteStopItem
-//	for _, routeStopItem := range routeItem.StopList {
-//		routeStopList = append(routeStopList, CreateRouteStopItem(routeStopItem.StopName, routeStopItem.TimetableList))
-//	}
-//	return RouteItemResponse{
-//		Name:        routeItem.Name,
-//		Description: RouteDescription{routeItem.DescriptionKorean, routeItem.DescriptionEnglish},
-//		StopList:    routeStopList,
-//	}
-//}
+func CreateStopItemResponse(stopItem shuttle.Stop) StopItemResponse {
+	var routeStopList []StopRouteItem
+	for _, routeStopItem := range stopItem.RouteList {
+		routeStopList = append(routeStopList, CreateStopRouteItem(routeStopItem.RouteName, routeStopItem.TimetableList))
+	}
+	return StopItemResponse{
+		Name:      stopItem.Name,
+		Location:  StopLocation{stopItem.Latitude, stopItem.Longitude},
+		RouteList: routeStopList,
+	}
+}
 
-//
-//func CreateRouteStopItem(stopName string, timetableList []shuttle.Timetable) RouteStopItem {
-//	return RouteStopItem{
-//		Name:          stopName,
-//		TimetableList: CreateTimetable(timetableList),
-//	}
-//}
-//
-//func CreateTimetable(timetableList []shuttle.Timetable) []string {
-//	var timetable = make([]string, 0)
-//	for _, timetableItem := range timetableList {
-//		timetable = append(timetable, fmt.Sprintf(
-//			"%02d:%02d",
-//			timetableItem.DepartureTime.Microseconds/1000000/60/60,
-//			timetableItem.DepartureTime.Microseconds/1000000/60%60,
-//		))
-//	}
-//	sort.Slice(timetable, func(i, j int) bool {
-//		return timetable[i] < timetable[j]
-//	})
-//	return timetable
-//}
-//
-//func CreateRouteLocationResponse(routeItem shuttle.Route) RouteLocationResponse {
-//	// 현재 시간 로딩 (KST)
-//	loc, _ := time.LoadLocation("Asia/Seoul")
-//	now := time.Now().In(loc)
-//
-//	cumulativeTimeList := make([]int, len(routeItem.StopList))
-//	for i, routeStopItem := range routeItem.StopList {
-//		cumulativeTimeList[i] = routeItem.StopList[len(routeItem.StopList)-1].CumulativeTime - routeStopItem.CumulativeTime
-//	}
-//	timetable := routeItem.StopList[len(routeItem.StopList)-1].TimetableList
-//	remainingTime := 0
-//	var location = make([]float64, 0)
-//	for _, timetableItem := range timetable {
-//		remainingTime = int((timetableItem.DepartureTime.Microseconds/1000000/60/60-int64(now.Hour()))*60 +
-//			(timetableItem.DepartureTime.Microseconds/1000000/60%60 - int64(now.Minute())))
-//		if remainingTime > cumulativeTimeList[0] {
-//			continue
-//		}
-//		idx := len(cumulativeTimeList)
-//		for i, cumulativeTime := range cumulativeTimeList {
-//			if remainingTime > cumulativeTime || (remainingTime == cumulativeTime && i > 0) {
-//				idx = i
-//				break
-//			}
-//		}
-//		location = append(location, float64(idx)-float64(remainingTime-cumulativeTimeList[idx])/float64(cumulativeTimeList[idx]-cumulativeTimeList[idx-1]))
-//	}
-//	sort.Slice(location, func(i, j int) bool {
-//		return location[i] < location[j]
-//	})
-//	return RouteLocationResponse{
-//		Location: location,
-//	}
-//}
+func CreateStopRouteItem(RouteName string, timetableList []shuttle.Timetable) StopRouteItem {
+	return StopRouteItem{
+		Name:          RouteName,
+		TimetableList: CreateTimetable(timetableList),
+	}
+}
--- rest/shuttle/stop.go
+++ rest/shuttle/stop.go
@@ -5,6 +5,7 @@ import (
 	model "github.com/hyuabot-developers/hyuabot-backend-golang/model/shuttle"
 	response "github.com/hyuabot-developers/hyuabot-backend-golang/response/shuttle"
 	"github.com/hyuabot-developers/hyuabot-backend-golang/util"
+	"time"
 )
 
 // 셔틀버스 정류장 목록 조회
@@ -16,7 +17,27 @@ func GetShuttleStopList(c *fiber.Ctx) error {
 
 // 셔틀버스 정류장 항목 조회
 func GetShuttleStopItem(c *fiber.Ctx) error {
-	return c.SendString("GetShuttleStopItem")
+	// 현재 시간 로딩 (KST)
+	loc, _ := time.LoadLocation("Asia/Seoul")
+	now := time.Now().In(loc)
+
+	var periodItem model.Period
+	result := util.DB.Database.Model(&model.Period{}).
+		Where("period_start <= ?", now).
+		Where("period_end >= ?", now).
+		First(&periodItem)
+
+	var stopItem model.Stop
+	result = util.DB.Database.Model(&model.Stop{}).
+		Preload("RouteList.TimetableList", "period_type = ? and departure_time >= ? and weekday = ?",
+			periodItem.Type, now, now.Weekday() < 6).
+		Where("shuttle_stop.stop_name = ?", c.Params("stop_id")).
+		First(&stopItem)
+	// 해당 노선 ID가 존재하지 않는 경우
+	if result.Error != nil {
+		return c.SendStatus(fiber.StatusNotFound)
+	}
+	return c.JSON(response.CreateStopItemResponse(stopItem))
 }
 
 // 셔틀버스 정류장 항목 추가
