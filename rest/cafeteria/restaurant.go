620eb5d227389f0f7eaa1e95340101bf1e948723
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-12-14 13:51:22 +0900

    feat: 학식 식당 항목 조회

--- rest/cafeteria/restaurant.go
+++ rest/cafeteria/restaurant.go
@@ -43,5 +43,32 @@ func GetRestaurantList(c *fiber.Ctx) error {
 
 // 식당 항목 조회
 func GetRestaurantItem(c *fiber.Ctx) error {
-	return c.SendString("GetRestaurantItem")
+	// 기준 날짜 로딩
+	loc, _ := time.LoadLocation("Asia/Seoul")
+	now := time.Now().In(loc)
+	date := c.Query("date")
+	if date == "" {
+		date = fmt.Sprintf("%d-%02d-%02d", now.Year(), now.Month(), now.Day())
+	}
+	// 조식, 중식, 석식 시간 로딩
+	timeType := c.Query("time")
+	if timeType == "" {
+		switch {
+		case now.Hour() < 10:
+			timeType = "조식"
+		case now.Hour() > 15:
+			timeType = "석식"
+		default:
+			timeType = "중식"
+		}
+	}
+
+	var restaurantItem model.RestaurantItem
+	util.DB.Database.Model(&model.Restaurant{}).
+		Preload("MenuList", "feed_date = ? and time_type like ?",
+			fmt.Sprintf(date),
+			fmt.Sprintf("%%%s%%", timeType)).
+		Where("campus_id = ? and restaurant_id = ?", c.Params("campus_id"), c.Params("restaurant_id")).
+		Find(&restaurantItem)
+	return c.JSON(response.CreateRestaurantItemResponse(restaurantItem))
 }
