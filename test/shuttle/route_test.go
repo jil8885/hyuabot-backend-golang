c859173340103fae4470ccf9f54b6977fd6e9e05
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-12-15 16:16:01 +0900

    feat: 셔틀버스 노선 항목 테스트

--- test/shuttle/route_test.go
+++ test/shuttle/route_test.go
@@ -37,3 +37,46 @@ func TestShuttleRouteList(t *testing.T) {
 		}
 	}
 }
+
+func TestShuttleRouteItem(t *testing.T) {
+	// Request Object
+	routeList := [3]string{"DH", "DY", "C"}
+	for _, route := range routeList {
+		request := httptest.NewRequest("GET", "/rest/shuttle/route/"+route, nil)
+		app := test.InitApp()
+
+		// Test
+		var resp shuttle.RouteItemResponse
+		response, err := app.Test(request)
+		if err != nil {
+			t.Error(err)
+		} else if response.StatusCode != 200 {
+			t.Error("Status code is not 200")
+		}
+		if err = json.NewDecoder(response.Body).Decode(&resp); err != nil {
+			t.Error(err)
+		}
+		if resp.Name == "" {
+			t.Error("RouteID is empty")
+		}
+		if resp.Description.Korean == "" || resp.Description.English == "" {
+			t.Error("Route Description is empty")
+		}
+		if resp.StopList == nil {
+			t.Error("StopList is nil")
+		}
+		for _, stop := range resp.StopList {
+			if stop.Name == "" {
+				t.Error("StopName is empty")
+			}
+			if stop.TimetableList == nil {
+				t.Error("TimetableList is nil")
+			}
+			for _, timetable := range stop.TimetableList {
+				if timetable == "" {
+					t.Error("Timetable is empty")
+				}
+			}
+		}
+	}
+}
