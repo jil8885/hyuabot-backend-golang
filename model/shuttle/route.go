dd150214b2139072c65803d2b678d59821032dae
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-12-20 15:07:20 +0900

    fix: 셔틀버스 모델 재구성

--- model/shuttle/route.go
+++ model/shuttle/route.go
@@ -4,6 +4,11 @@ type Route struct {
 	Name               string      `gorm:"column:route_name;primaryKey"`
 	DescriptionKorean  string      `gorm:"column:route_description_korean"`
 	DescriptionEnglish string      `gorm:"column:route_description_english"`
+	Tag                string      `gorm:"column:route_tag"`
+	StartStopID        string      `gorm:"column:start_stop"`
+	EndStopID          string      `gorm:"column:end_stop"`
+	StartStop          Stop        `gorm:"foreignKey:StopID;references:StartStopID"`
+	EndStop            Stop        `gorm:"foreignKey:StopID;references:EndStopID"`
 	StopList           []RouteStop `gorm:"foreignKey:RouteName;references:Name"`
 }
 
@@ -11,6 +16,11 @@ type RouteItem struct {
 	Name               string `gorm:"column:route_name;primaryKey"`
 	DescriptionKorean  string `gorm:"column:route_description_korean"`
 	DescriptionEnglish string `gorm:"column:route_description_english"`
+	Tag                string `gorm:"column:route_tag"`
+	StartStopID        string `gorm:"column:start_stop"`
+	EndStopID          string `gorm:"column:end_stop"`
+	StartStop          Stop   `gorm:"foreignKey:StopID;references:StartStopID"`
+	EndStop            Stop   `gorm:"foreignKey:StopID;references:EndStopID"`
 }
 
 func (Route) TableName() string {
