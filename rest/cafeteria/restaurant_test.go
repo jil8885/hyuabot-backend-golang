6ddd29282d0c501384de755106ea264aa580ade0
Jeongin Lee <jil8885@hanyang.ac.kr>
2023-03-23 18:10:19 +0900

    test: 학식 식당 항목 조회

--- rest/cafeteria/restaurant_test.go
+++ rest/cafeteria/restaurant_test.go
@@ -59,3 +59,51 @@ func TestGetRestaurantList(t *testing.T) {
 		}
 	}
 }
+
+func TestGetRestaurantItem(t *testing.T) {
+	test := assert.New(t)
+	// Get all bus routes
+	t.Log("TestGetRestaurantItem")
+	util.ConnectDB()
+	app := fiber.New()
+	app.Get("/rest/cafeteria/campus/:campus_id/restaurant/:restaurant_id", GetRestaurantItem)
+	restaurantList := map[int][]int{
+		0: {1, 2, 4, 5, 7, 8},
+		1: {11, 12, 13, 15},
+	}
+	for campus, restaurants := range restaurantList {
+		for _, restaurant := range restaurants {
+			for i := -5; i < 5; i++ {
+				date := carbon.Now("Asia/Seoul").AddDays(i).Format("Y-m-d")
+				for _, time := range []string{"조식", "중식", "석식"} {
+					request := httptest.NewRequest("GET", fmt.Sprintf("/rest/cafeteria/campus/%d/restaurant/%d?date=%s&type=%s", campus, restaurant, date, time), nil)
+					res, err := app.Test(request)
+					test.Nil(err)
+					test.Equal(200, res.StatusCode)
+					body, err := io.ReadAll(res.Body)
+					test.Nil(err)
+					var obj cafeteria.RestaurantListResponse
+					err = json.Unmarshal(body, &obj)
+					test.Nil(err)
+					test.IsType([]cafeteria.RestaurantItemResponse{}, obj.RestaurantList)
+					for _, restaurant := range obj.RestaurantList {
+						test.IsType(cafeteria.RestaurantItemResponse{}, restaurant)
+						test.IsType(0, restaurant.RestaurantID)
+						test.IsType("", restaurant.Name)
+						test.IsType([]cafeteria.MenuList{}, restaurant.MenuList)
+						for _, menu := range restaurant.MenuList {
+							test.IsType(cafeteria.MenuList{}, menu)
+							test.IsType("", menu.TimeType)
+							test.IsType([]cafeteria.MenuItem{}, menu.MenuList)
+							for _, menuItem := range menu.MenuList {
+								test.IsType(cafeteria.MenuItem{}, menuItem)
+								test.IsType("", menuItem.Food)
+								test.IsType("0", menuItem.Price)
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
